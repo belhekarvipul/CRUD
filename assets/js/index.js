10a4cd7b8c4b3a1730cfee0d7d1c213fd9a0653c
Vipul Belhekar <belhekarvipul@gmail.com>
2023-06-28 21:37:26 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
 	$("#btnTest").click(function(){
-		alert("Button clicked.");
+		//alert("Button clicked.");	
+		
+		fetch("https://belhekarvipul.github.io/CRUD/assets/data.json")
+        .then((res) => {
+			return res.json();
+			})
+		.then((data) => console.log(data));
 	});
-});
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-		}
-    }
-    rawFile.send(null);
-}
-
-readTextFile("assets/data.json", function(text){
-    //var data = JSON.parse(text);
-    //console.log(data);
 });
