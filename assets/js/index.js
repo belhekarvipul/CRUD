2417e454097e7e6159709d38babbe96a7d6e6d72
Vipul Belhekar <belhekarvipul@gmail.com>
2023-06-28 22:08:00 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-	$("#btnTest").click(function(){
-		//alert("Button clicked.");	
+	var records;
+	var url = 'https://belhekarvipul.github.io/CRUD/';
+	
+	fetch(url + "assets/data.json").then((res) => { return res.json(); }).then((data) => {records = data;});
 		
-		fetch("https://belhekarvipul.github.io/CRUD/assets/data.json")
-        .then((res) => {
-			return res.json();
-			})
-		.then((data) => console.log(data));
+	$("#btnTest").click(function(){
+		$("#tblAllData tbody tr").remove();
+		
+		$.each(records, function( index, value ) {
+			$('#tblAllData > tbody:last-child')
+				.append('<tr>' +
+					'<td>' + value.srno + '</td>' +
+					'<td>' + value.fname + '</td>' +
+					'<td>' + value.lname + '</td>' +
+					'<td>' + value.address + '</td>' +
+					'<td>' + value.dob + '</td>' +
+					'<td>' + value.status + '</td></tr>');
+		});
 	});
 });
